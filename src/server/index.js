895eec233668f40ea919b785ef980a1d89e1f140
Unknown <martez_conner@outlook.com>
2018-01-05 18:12:25 -0800

    Changed server file

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,22 @@
-const app = require('http').createServer()
-const io = module.exports.io = require('socket.io')(app)
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app)
+const io = module.exports.io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 5000
 
 const SocketManager = require('./SocketManager')
 
 io.on('connection', SocketManager)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server Starts on ${PORT}`);
