9dc7d240e2d8d67e96309c493756c488670af8be
Unknown <martez_conner@outlook.com>
2018-01-05 18:03:58 -0800

    .

--- src/server/index.js
+++ src/server/index.js
@@ -7,15 +7,6 @@ const SocketManager = require('./SocketManager')
 
 io.on('connection', SocketManager)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-}
-
 app.listen(PORT, () => {
   console.log(`Server Starts on ${PORT}`);
 });
