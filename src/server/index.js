485ba7d71d73c63e80e2d8e7807c4c234c647f69
Unknown <martez_conner@outlook.com>
2018-01-06 15:04:42 -0800

    Changed Home component

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 5000
 
 const server = http.createServer(app)
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
 
